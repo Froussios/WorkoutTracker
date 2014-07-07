bfe2a6aa8486d7ca293f2f5b0c2b0783234ca804
Chris Froussios <darkmatter.tm@gmail.com>
2014-07-07 22:08:59 +0200

    ActivityViewer shows graphs

--- WorkoutTracker/Views/ActivityViewer.xaml.cs
+++ WorkoutTracker/Views/ActivityViewer.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Net;
 using System.Windows;
@@ -10,8 +11,20 @@ using Microsoft.Phone.Shell;
 
 namespace WorkoutTracker
 {
-    public partial class ActivityViewer : PhoneApplicationPage
+    public partial class ActivityViewer : PhoneApplicationPage, INotifyPropertyChanged
     {
+        public Activity DataContext
+        {
+            get { return base.DataContext as Activity; }
+            set
+            {
+                base.DataContext = value;
+                NotifyPropertyChanged("DailyTotals");
+                NotifyPropertyChanged("DailyAverages");
+                NotifyPropertyChanged("DailyMaximums");
+            }
+        }
+
         public ActivityViewer()
         {
             InitializeComponent();
@@ -27,14 +40,95 @@ namespace WorkoutTracker
                 this.DataContext = activity;
 
                 IEnumerable<Entry> entries = App.ViewModel.GetEntries(activity);
-                if (entries.Count(x => true) > 0)
+                if (entries.Count() > 0)
                 {
                     HistoryEntriesList.ItemsSource = entries;
                     TotalTextBlock.Text = entries.Sum(x => x.Count).ToString();
                     AverageSetTextBlock.Text = entries.Average(x => x.Count).ToString();
                 }
+
+                AverageGraph.Data = this.DailyAverages;
+                MaximumGraph.Data = this.DailyMaxima;
+                TotalGraph.Data = this.DailyTotals;
             }
         }
+
+        private static DateTime Today 
+        {
+            get { return DateTime.Today; }
+        }
+        private static DateTime LastMonth
+        {
+            get { return DateTime.Today.AddMonths(-1); }
+        }
+
+
+
+        public class MyGrouping<TKey, TElement> : List<TElement>, IGrouping<TKey, TElement>
+        {
+            public TKey Key { get; set; }
+        }
+
+        public IEnumerable<IGrouping<DateTime, Entry>> MonthDummies =
+            Enumerable.Range(0, 1 + Today.Subtract(LastMonth).Days)
+                .Select(offset => LastMonth.AddDays(offset))
+                .Select(date => new MyGrouping<DateTime, Entry> {Key = date})
+                .ToArray();
+
+        public class C : IEqualityComparer<IGrouping<DateTime, Entry>>
+        {
+            public bool Equals(IGrouping<DateTime, Entry> x, IGrouping<DateTime, Entry> y)
+            {
+                return x.Key.Equals(y.Key);
+            }
+
+            public int GetHashCode(IGrouping<DateTime, Entry> obj)
+            {
+                return obj.Key.GetHashCode();
+            }
+        }
+
+        public IEnumerable<double> DailyTotals
+        {
+            get
+            {
+                return App.ViewModel.EntriesLastMonth
+                                    .Where(entry => entry.Activity.Equals(DataContext))
+                                    .GroupBy(entry => entry.Date.Date)
+                                    .Union(MonthDummies, new C())
+                                    .OrderBy(group => group.Key)
+                                    .Select(group => (double)(group.Count() > 0 ? group.Sum(entry => entry.Count) : 0));
+            }
+        }
+
+
+        public IEnumerable<double> DailyAverages
+        {
+            get
+            {
+                return App.ViewModel.EntriesLastMonth
+                                    .Where(entry => entry.Activity.Equals(DataContext))
+                                    .GroupBy(entry => entry.Date.Date)
+                                    .Union(MonthDummies, new C())
+                                    .OrderBy(group => group.Key)
+                                    .Select(group => group.Count() > 0 ? group.Average(entry => entry.Count) : 0);
+            }
+        }
+
+
+        public IEnumerable<double> DailyMaxima
+        {
+            get
+            {
+                return App.ViewModel.EntriesLastMonth
+                                    .Where(entry => entry.Activity.Equals(DataContext))
+                                    .GroupBy(entry => entry.Date.Date)
+                                    .Union(MonthDummies, new C())
+                                    .OrderBy(group => group.Key)
+                                    .Select(group => (double)(group.Count() > 0 ? group.Max(entry => entry.Count) : 0));
+            }
+        }
+
 
         private void Delete_Click(object sender, EventArgs e)
         {
@@ -82,5 +176,21 @@ namespace WorkoutTracker
             base.OnNavigatedFrom(e);
             SaveChanges();
         }
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Used to notify that a property changed
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
     }
 }
